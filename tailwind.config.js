b5acadbabc7b3ad31ab84a3730b9a84a6c59ddf1
Ryuki Isami <isamin1211.program@gmail.com>
2025-05-19 14:14:51 +0900

    20250519_add:tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',  // これでビューのHTMLファイルを監視します
+    './app/helpers/**/*.rb',      // ヘルパーファイルも監視対象に
+    './app/javascript/**/*.js'   // JavaScriptファイルも監視対象に
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
