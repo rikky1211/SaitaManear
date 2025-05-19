20502e9333fb0493374400e99bcbb61c0e788c09
Ryuki Isami <isamin1211.program@gmail.com>
2025-05-19 16:14:41 +0900

    20250519_add:spots

---
+++ app/models/spot.rb
@@ -0,0 +1,12 @@
+class Spot < ApplicationRecord
+  # geocoded_by :address
+  # after_validation :geocode
+
+  validates :spot_image, presence: true
+  validates :latitude, presence: true, uniqueness: true
+  validates :longitude, presence: true, uniqueness: true
+
+  belongs_to :user
+  belongs_to :town
+  has_one_attached :spot_image
+end
---
+++ db/migrate/20250519071059_create_spots.rb
@@ -0,0 +1,18 @@
+class CreateSpots < ActiveRecord::Migration[7.2]
+  def change
+    create_table :spots, id: false do |t|
+      t.uuid :id, primary_key: true, default: -> { "gen_random_uuid()" }
+      t.references :user, null: false, type: :uuid, foreign_key: true
+      t.references :town, null: false, foreign_key: true
+      t.string :name, null: false
+      t.string :spot_image
+      t.string :address
+      t.text :opening_hours
+      t.text :summary
+      t.float :latitude, null: false
+      t.float :longitude, null: false
+
+      t.timestamps
+    end
+  end
+end
