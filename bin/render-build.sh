b690a80ef980275b212746898766330135af2cc3
Ryuki Isami <isamin1211.program@gmail.com>
2025-05-20 20:29:13 +0900

    202505020_fix:initializers_assets.rb修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,9 @@
 set -o errexit
 
 bundle install
+yarn install
+yarn build                # JSをビルド（esbuild）
+yarn build:css            # ←★ Tailwind CSSをビルド（これがなかった）
+
 bundle exec rails assets:precompile
-bundle exec rails assets:clean
 bundle exec rails db:migrate
