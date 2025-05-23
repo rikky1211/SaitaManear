28fe86eee70abc4cc903028f8f072c20d7e943d9
Ryuki Isami <isamin1211.program@gmail.com>
2025-05-23 18:31:33 +0900

    20250523_add:domain用ファイル作成

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,7 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.hosts << 'www.saitamanear.com'
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
