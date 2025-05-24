92a82ad1fc5044d9e211392257e3502c7cc2f4c5
Ryuki Isami <isamin1211.program@gmail.com>
2025-05-25 01:46:26 +0900

    20250525_add:spots/フラッシュメッセージ

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,11 +2,20 @@ class Spot < ApplicationRecord
   # geocoded_by :address
   # after_validation :geocode
 
+  validates :name, presence: true
   validates :spot_image, presence: true
-  validates :latitude, presence: true, uniqueness: true
-  validates :longitude, presence: true, uniqueness: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+
+  validate :LatLng_uniq
 
   belongs_to :user
   # belongs_to :town
   has_one_attached :spot_image
+
+  def LatLng_uniq
+    if Spot.where(latitude: latitude, longitude: longitude).exists?
+      errors.add(:base, "同じ緯度・経度のスポットはすでに存在します。")
+    end
+  end
 end
