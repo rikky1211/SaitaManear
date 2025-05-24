cbc9eb79021aaeb1b15a9fbd5ec0989472604f60
Ryuki Isami <isamin1211.program@gmail.com>
2025-05-25 01:34:44 +0900

    20250525_add:スポット作成・編集に合わせて飛ばす先を変更できるように修正-02

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :form_url
+
+  def form_url
+    if controller_path == "your_spots"
+      your_spot_path(@spot)
+    else
+      spots_path
+    end
+  end
 end
