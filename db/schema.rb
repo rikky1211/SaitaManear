7676fe46409d3a46aa0826715439e65eb581bf3d
Ryuki Isami <isamin1211.program@gmail.com>
2025-06-26 13:45:26 +0900

    20250626_add:Google認証_Userテーブル/カラムunique-SQLでの名前付

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_26_040417) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_26_042603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -82,6 +82,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_26_040417) do
     t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
