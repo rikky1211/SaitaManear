2f2af531a6a64e9ec5dbacaa35d8de178ddaf1fd
Ryuki Isami <isamin1211.program@gmail.com>
2025-07-07 07:47:22 +0900

    20260707_fix:default行追加

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
+# -----------------------------------
+# default
+# -----------------------------------
   # GET /resource/password/new
   # def new
   #   super
