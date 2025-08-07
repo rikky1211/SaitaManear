eb573f7b49dcd83b4c64fb3028d9e813388de8ca
Ryuki Isami <isamin1211.program@gmail.com>
2025-08-07 19:18:59 +0900

    20250807_add:利用規約

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,12 +3,12 @@ class TopController < ApplicationController
     # with_attached_spot_image: 画像が添付されているSpotのみ取得
     # sample: ランダムで1つ選ぶ
     @background_spot = Spot.with_attached_spot_image { |s| s.spot_image.attached? }.sample
-    puts "-----------------------------------"
-    puts @background_spot
-    puts "-----------------------------------"
   end
 
   def how_to_use
+  end
+
+  def terms_of_use
   end
 
   def sorry
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
   content: [
     './app/views/**/*.html.erb',  // これでビューのHTMLファイルを監視します
     './app/helpers/**/*.rb',      // ヘルパーファイルも監視対象に
-    './app/javascript/**/*.js'   // JavaScriptファイルも監視対象に
-
+    './app/javascript/**/*.js',   // JavaScriptファイルも監視対象に
+    './app/assets/stylesheets/*.css'
   ],
 
   plugins: [require("daisyui")],
