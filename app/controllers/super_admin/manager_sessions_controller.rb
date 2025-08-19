c0775ead31c457382ffd244b3f434d8b34df4a31
Ryuki Isami <isamin1211.program@gmail.com>
2025-08-20 04:04:20 +0900

    20250820_fix:baseコントローラからのメソッド継承

--- app/controllers/super_admin/manager_sessions_controller.rb
+++ app/controllers/super_admin/manager_sessions_controller.rb
@@ -1,4 +1,4 @@
-class SuperAdmin::ManagerSessionsController < ApplicationController
+class SuperAdmin::ManagerSessionsController < SuperAdmin::BaseController
   layout "super_admin/layouts/application"
   before_action :authenticate_user!, only: %i[new]
   def new; end
--- app/controllers/super_admin/users_controller.rb
+++ app/controllers/super_admin/users_controller.rb
@@ -1,4 +1,4 @@
-class SuperAdmin::UsersController < ApplicationController
+class SuperAdmin::UsersController < SuperAdmin::BaseController
   layout "super_admin/layouts/application"
   before_action :authenticate_user!, only: %i[show edit update]
   def index; end
