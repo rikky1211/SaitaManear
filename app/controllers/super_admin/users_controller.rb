680ca3215bb6f2ae06ad89c2af6b983e8a6851ce
Ryuki Isami <isamin1211.program@gmail.com>
2025-08-20 06:34:53 +0900

    20250820_add:SuperAdmin_userページ作成

--- app/controllers/super_admin/users_controller.rb
+++ app/controllers/super_admin/users_controller.rb
@@ -1,5 +1,23 @@
 class SuperAdmin::UsersController < SuperAdmin::BaseController
   layout "super_admin/layouts/application"
-  before_action :authenticate_user!, only: %i[show edit update]
-  def index; end
+  before_action :authenticate_user!, only: %i[index show edit update destroy]
+  def index
+    @users = User.all
+  end
+
+  def show
+
+  end
+
+  def edit
+    
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end 
+
 end
