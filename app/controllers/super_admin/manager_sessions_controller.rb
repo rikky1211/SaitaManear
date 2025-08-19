92f92bf9f640d6c3edd74840cc7a6a3a7cac2b62
Ryuki Isami <isamin1211.program@gmail.com>
2025-08-20 05:39:11 +0900

    20250820_delete:SuperAdmin_ログインページ削除(いらないと判断した為)

--- app/controllers/super_admin/manager_sessions_controller.rb
+++
@@ -1,5 +0,0 @@
-class SuperAdmin::ManagerSessionsController < SuperAdmin::BaseController
-  layout "super_admin/layouts/application"
-  before_action :authenticate_user!, only: %i[new]
-  def new; end
-end
