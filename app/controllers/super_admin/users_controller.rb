22a62d74dbe50a63f0b08d2e13a15819eaa70011
Ryuki Isami <isamin1211.program@gmail.com>
2025-08-20 18:13:23 +0900

    20250820_add:ユーザー情報修正ページ

--- app/controllers/super_admin/users_controller.rb
+++ app/controllers/super_admin/users_controller.rb
@@ -10,17 +10,30 @@ class SuperAdmin::UsersController < SuperAdmin::BaseController
   end
 
   def edit
-    
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to super_admin_users_path, notice: "成功：ユーザ情報を修正しました"
+    else
+      flash.now[:error] = "失敗：ユーザ情報を修正できませんでした"
+      render :edit, status: :unprocessable_entity
+    end
 
   end
 
   def destroy
     user = User.find(params[:id])
     user.destroy!
-    redirect_to super_admin_users_path, notice: "ユーザを削除しました"
+    redirect_to super_admin_users_path, notice: "成功：ユーザ情報を削除しました"
   end 
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:id, :name, :email, :role)
+  end
 
 end
